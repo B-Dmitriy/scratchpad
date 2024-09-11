a103720ab3d1f25f7484d413b4a911719675c8ba
d.balyaev <d.balyaev@npo-echelon.ru>
2024-09-11 14:18:49 +0300

    fix: supertest cases

--- server/src/__test__/controllers/tasks.controller.test.ts
+++ server/src/__test__/controllers/tasks.controller.test.ts
@@ -8,7 +8,7 @@ afterAll(() => {
 })
 
 describe('tasks controller', () => {
-    it('test', (done) => {
+    it('should be validation wrong (page string)', (done) => {
         request(app)
             .post('/tasks:list')
             .send({ page: 'wrong', limit: 20 })
@@ -17,10 +17,16 @@ describe('tasks controller', () => {
                 res.body.message = 'validation error';
                 res.body.errors.length = 1
             })
-            .send({ page: '1', limit: 20 })
+            .end(done)
+    })
+
+    it('should be validation wrong (page negative)', (done) => {
+        request(app)
+            .post('/tasks:list')
+            .send({ page: -1, limit: 20 })
             .expect(400)
             .expect((res) => {
-                res.body.id = 'validation error';
+                res.body.message = 'validation error';
                 res.body.errors.length = 1
             })
             .end(done)
